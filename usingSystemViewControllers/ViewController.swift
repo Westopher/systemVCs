a9ff709867ba3618d6179ecccaf134d9e60a2984
West Kraemer <kraemerwest@gmail.com>
2019-05-20 15:16:21 -0400

    add styling to buttons

--- usingSystemViewControllers/ViewController.swift
+++ usingSystemViewControllers/ViewController.swift
@@ -10,9 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //button names for styling
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var safariButton: UIButton!
+    @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var emailButton: UIButton!
+    
+    
+    
+    //button actions
+    @IBAction func shareButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func safariButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cameraButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func emailButtonTapped(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        self.shareButton.layer.borderColor = UIColor.blue.cgColor
+self.shareButton.layer.cornerRadius = 3
+//
+//        self.safariButton.layer.borderColor = UIColor.blue.cgColor
+self.safariButton.layer.cornerRadius = 3
+//
+//        self.cameraButton.layer.borderColor = UIColor.blue.cgColor
+self.cameraButton.layer.cornerRadius = 3
+//
+//        self.emailButton.layer.borderColor = UIColor.blue.cgColor
+self.emailButton.layer.cornerRadius = 3
+        
     }
 
 
